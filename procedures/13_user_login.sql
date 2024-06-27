49cbd8907bd0ab6e69d5c33e5167020c444b0ebf
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-06-27 14:11:44 -0300

    add professional_login

--- procedures/13_user_login.sql
+++ procedures/13_user_login.sql
@@ -16,5 +16,23 @@ BEGIN
 END;
 $function$
 
+CREATE OR REPLACE FUNCTION public.professional_login(professional_email character varying, professional_password character varying)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    was_found INTEGER;
+BEGIN 
+    SELECT p.professional_id INTO was_found
+    FROM "professional" p 
+    WHERE p.email = professional_email
+    AND p.password = crypt(professional_password, password);
+    
+    IF was_found IS NULL THEN RETURN 0; END IF;
+
+    RETURN was_found;
+END;
+$function$
+
 
 -- SELECT user_login('baig', '43721804');
