3f0e47d7eb58baa7f950d89214d28027717f23c5
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-06-27 14:32:30 -0300

    correcion de errores

--- procedures/13_user_login.sql
+++ procedures/13_user_login.sql
@@ -16,7 +16,11 @@ BEGIN
 END;
 $function$;
 
-CREATE OR REPLACE FUNCTION public.professional_login(professional_email character varying, professional_password character varying)
+CREATE OR REPLACE FUNCTION public.professional_login(
+	professional_email character varying DEFAULT NULL,
+	professional_phone character varying DEFAULT NULL,
+	professional_password character varying DEFAULT NULL
+)
  RETURNS integer
  LANGUAGE plpgsql
 AS $function$
@@ -25,7 +29,7 @@ DECLARE
 BEGIN 
     SELECT p.professional_id INTO was_found
     FROM "professional" p 
-    WHERE p.email = professional_email
+    WHERE (p.email = professional_email OR p.phone = professional_phone)
     AND p.password = crypt(professional_password, password);
     
     IF was_found IS NULL THEN RETURN 0; END IF;
@@ -33,6 +37,5 @@ BEGIN
     RETURN was_found;
 END;
 $function$;
-
 
 -- SELECT user_login('baig', '43721804');
